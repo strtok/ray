4ef56fcca4f79517c2d9caeda8e26024d599f873
Erik Bremen <strtok@strtok.net>
2018-10-12 09:20:53 -0700

    Render upside down image

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate pretty_env_logger;
 use piston_window::*;
 
 fn main() {
+    pretty_env_logger::init();
 
     let opengl = OpenGL::V3_2;
     let (width, height) = (640, 480);
@@ -23,9 +24,20 @@ fn main() {
         &TextureSettings::new()
     ).unwrap();
 
+    for y in (0..height-1).rev() {
+        for x in 0..width-1 {
+            let r: f32 = x as f32 / width as f32;
+            let g: f32 = y as f32 / height as f32;
+            let b: f32 = 0.2;
+            let ir = (255.99 as f32 * r) as u8;
+            let ig = (255.99 as f32 * g) as u8;
+            let ib = (255.99 as f32 * b) as u8;
+            canvas.put_pixel(x, y, im::Rgba([ir, ig, ib, 255]));
+        }
+    }
+
     while let Some(e) = window.next() {
         if let Some(_) = e.render_args() {
-            debug!("rendering");
             texture.update(&mut window.encoder, &canvas).unwrap();
             window.draw_2d(&e, |c, g| {
                 clear([1.0; 4], g);
