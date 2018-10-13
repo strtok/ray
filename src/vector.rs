6b94ce763c841121c68fe4829ad739f4ab8f675d
Erik Bremen <strtok@strtok.net>
2018-10-12 20:50:56 -0700

    Add Vector::from

--- src/vector.rs
+++ src/vector.rs
@@ -5,6 +5,10 @@ pub struct Vector {
 }
 
 impl Vector {
+    fn from(x: f32, y: f32, z: f32) -> Vector {
+        return Vector{x: x, y: y, z: z};
+    }
+
     fn length(&self) -> f32 {
         return (self.x*self.x + self.y*self.y + self.z*self.z).sqrt();
     }
@@ -19,7 +23,7 @@ mod tests {
     use vector::*;
     #[test]
     fn length() {
-        let v = Vector {x: 1.0, y: 1.0, z: 1.0};
+        let v = Vector::from(1.0, 1.0, 1.0);
         assert!(v.length() > 1.0);
     }
 }
